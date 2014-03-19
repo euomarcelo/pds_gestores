0e50872a7080d668437b1b9f2d3d3f8f603c66dc
Marcelo E. Cardoso <marc.cardo@gmail.com>
2014-03-19 18:04:19 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PdsGestores::Application.config.secret_key_base = '6f60e6959eb72eba1360da348c6d7e54378c35aa6992339da349c7100851eae9940a1e33b1a316da2a56f56936cedd735f03d5630488014b2c3e7280b7bd3bed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PdsGestores::Application.config.session_store :cookie_store, key: '_pds_gestores_session'
